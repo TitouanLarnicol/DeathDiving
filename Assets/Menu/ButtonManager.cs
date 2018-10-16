714eaf267f6e1f8f549efa3004c035b78816b9fd
TitouanLarnicol <larnicol.titouan@orange.fr>
2018-10-17 01:50:11 +0200

    FINAL VERSION

--- Assets/Menu/ButtonManager.cs
+++ Assets/Menu/ButtonManager.cs
@@ -8,7 +8,7 @@ public class ButtonManager : MonoBehaviour {
 	public void NewGameBtn(string newGameLevel){
 		SceneManager.LoadScene(newGameLevel);
 	}
-	public void ExitGameBtn(){
-		Application.Quit();
+	public void goFreeMode(){
+		SceneManager.LoadScene("FreeWorld");
 	}
 }
